673737e6b82529b4ec7a4278a7e0677adc94d3b3
Bram Reuling <bram.reuling@outlook.com>
2026-02-22 00:38:32 +0100

    Added: Custom Async BP Node.

---
+++ Source/UEExperiments/CustomNodes/Async/DemoAsyncNode.cpp
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2026 Bram Reuling
+
+#include "DemoAsyncNode.h"
+
+//======================================================================================================================
+UDemoAsyncNode* UDemoAsyncNode::DemoBlueprintNode(const UObject* WorldObject)
+{
+	UDemoAsyncNode* Node = NewObject<UDemoAsyncNode>();
+	if (Node)
+	{
+		Node->WorldObject = WorldObject;
+		Node->DelayTime = 2.f;
+	}
+	
+	return Node;
+}
+
+//======================================================================================================================
+void UDemoAsyncNode::Activate()
+{
+	InternalDirectCall();
+	
+	if (!WorldObject)
+		return;
+	
+	TimerDelegate.BindUObject(this, &UDemoAsyncNode::InternalLaterCall, DelayTime);
+	WorldObject->GetWorld()->GetTimerManager().SetTimer(TimerHandle, TimerDelegate, DelayTime, false);
+}
+
+//======================================================================================================================
+void UDemoAsyncNode::InternalDirectCall() const
+{
+	Direct.Broadcast(0.f);
+}
+
+//======================================================================================================================
+void UDemoAsyncNode::InternalLaterCall(float timeElapsed)
+{
+	Later.Broadcast(timeElapsed);
+	
+	WorldObject->GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+	TimerHandle.Invalidate();
+	TimerDelegate.Unbind();
+}
---
+++ Source/UEExperiments/CustomNodes/Async/DemoAsyncNode.h
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2026 Bram Reuling
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "DemoAsyncNode.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FDemoOutputPin, float, DemoOutput);
+
+/**
+ * 
+ */
+UCLASS()
+class UEEXPERIMENTS_API UDemoAsyncNode : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+	
+public:
+	float DelayTime = 0.f;
+	
+	UPROPERTY(BlueprintAssignable)
+	FDemoOutputPin Direct;
+	
+	UPROPERTY(BlueprintAssignable)
+	FDemoOutputPin Later;
+	
+private:
+	const UObject* WorldObject;
+	
+	FTimerDelegate TimerDelegate;
+	FTimerHandle TimerHandle;
+	
+public:
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", WorldContext = "WorldObject"), Category = "CustomNodes/Async/Demo")
+	static UDemoAsyncNode* DemoBlueprintNode(const UObject* WorldObject);
+	
+	virtual void Activate() override;
+	
+private:
+	UFUNCTION()
+	void InternalDirectCall() const;
+	
+	UFUNCTION()
+	void InternalLaterCall(float timeElapsed);
+};
