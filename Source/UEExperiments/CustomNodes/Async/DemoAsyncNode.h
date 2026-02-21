bba8d52177cea61833374f8f6f2f5933874f635a
Bram Reuling <bram.reuling@outlook.com>
2026-02-22 00:45:40 +0100

    Added: Comments to Async BP Node code.

--- Source/UEExperiments/CustomNodes/Async/DemoAsyncNode.h
+++ Source/UEExperiments/CustomNodes/Async/DemoAsyncNode.h
@@ -9,7 +9,15 @@
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FDemoOutputPin, float, DemoOutput);
 
 /**
+ * Creates a node that has a single incoming execution pin and multiple outgoing execution pins.
+ * The class has the following outgoing pins:
+ * - Direct: Executes directly after the incoming execution pin is triggered.
+ * - Later: Executes 2 seconds after the incoming execution pin is triggered.
  * 
+ * DemoBlueprintNode(...) is the actual 'Node' that you will call in blueprints. This function does the initial setup.
+ * Activate(...) will trigger the outgoing execution pins.
+ * 
+ * More information => https://blog.bramreuling.com/posts/Creating-async-blueprint-nodes-in-Unreal-Engine/
  */
 UCLASS()
 class UEEXPERIMENTS_API UDemoAsyncNode : public UBlueprintAsyncActionBase
@@ -26,8 +34,9 @@ public:
 	FDemoOutputPin Later;
 	
 private:
-	const UObject* WorldObject;
 	
+	// Timer.
+	const UObject* WorldObject;
 	FTimerDelegate TimerDelegate;
 	FTimerHandle TimerHandle;
 	
